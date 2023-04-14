3dae7ccfe5c1b4e821112fbe9cd732e1264572b3
jazzpujols34 <ro5112@hotmail.com>
2023-04-14 15:32:21 +0800

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,48 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
-import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
-
+import { Refine } from "@refinedev/core";
 import routerBindings, {
-  UnsavedChangesNotifier,
+    UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
 import dataProvider from "@refinedev/simple-rest";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HeadlessInferencer } from "@refinedev/inferencer/headless";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <GitHubBanner />
-      <RefineKbarProvider>
-        <Refine
-          routerProvider={routerBindings}
-          dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
-          options={{
-            syncWithLocation: true,
-            warnWhenUnsavedChanges: true,
-          }}
-        >
-          <Routes>
-            <Route index element={<WelcomePage />} />
-          </Routes>
-          <RefineKbar />
-          <UnsavedChangesNotifier />
-        </Refine>
-      </RefineKbarProvider>
-    </BrowserRouter>
-  );
-}
-
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Refine
+                routerProvider={routerBindings}
+                dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+                resources={[
+                    {
+                        name: "blog_posts",
+                        list: "/blog-posts",
+                        show: "/blog-posts/show/:id",
+                        create: "/blog-posts/create",
+                        edit: "/blog-posts/edit/:id",
+                    },
+                ]}
+                options={{
+                    syncWithLocation: true,
+                    warnWhenUnsavedChanges: true,
+                }}
+            >
+                <Routes>
+                    <Route path="blog-posts">
+                        <Route index element={<HeadlessInferencer />} />
+                        <Route
+                            path="show/:id"
+                            element={<HeadlessInferencer />}
+                        />
+                        <Route
+                            path="edit/:id"
+                            element={<HeadlessInferencer />}
+                        />
+                        <Route path="create" element={<HeadlessInferencer />} />
+                    </Route>
+                </Routes>
+                <UnsavedChangesNotifier />
+            </Refine>
+        </BrowserRouter>
+    );
+};
 export default App;
